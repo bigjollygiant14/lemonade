89a1228f022f3ee2ad9ec06fe52757bd471896da
wjay <wjay@deloitte.com>
2017-02-15 09:12:12 -0700

    finished user api routes

--- _server/routes/users.js
+++ _server/routes/users.js
@@ -5,14 +5,7 @@ const express = require( 'express' ),
       mongoose = require( 'mongoose'),
       Users = require( '../models/Users.js' );
 
-// module.exports = function(app) {
-  /* app.get('/api/users', function(req, res, next) {
-    Users.find(function (err, users) {
-      if (err) return next(err);
-      console.log('SHIT', users);
-      res.json(users);
-    });
-  }); */
+/* /api/users */
 router.get( '/api/users', function(req, res) {
   Users.find( function(err, users) {
     if (err) return next( err );
@@ -20,52 +13,33 @@ router.get( '/api/users', function(req, res) {
   });
 });
 
-router.post( '/api/users', function(req, res, next) {
-  // User.create(req.body, function (err, post) {
-    // if (err) return next(err);
-    // res.json(post);
-  // });
-  console.log( 'POSTING SHIT BIRD' );
+router.post( '/api/users', function( req, res, next ) {
+  Users.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
-// };
+
+/* /api/users/:id */
+router.get( '/api/users/:id', function( req, res ) {
+  Users.find( {_id: req.params.id}, function( err, users ) {
+    if ( err ) return next( err );
+    res.json( users );
+  });
+});
+
+router.delete( '/api/users/:id', function( req, res, next ) {
+  Users.findByIdAndRemove(req.params.id, req.body, function ( err, post ) {
+    if ( err ) return next( err )
+    res.json( post )
+  });
+});
+
+router.put( '/api/users/:id', function( req, res, next ) {
+  Users.findByIdAndUpdate(req.params.id, req.body, function ( err, post ) {
+    if ( err ) return next( err );
+    res.json( post );
+  });
+});
 
 module.exports = router;
-
-/* router.delete('/:id', function(req, res, next) {
-  User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-router.post('/', function(req, res, next) {
-  User.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-}); */
-
-/*
-router.get('/', function(req, res, next) {
-  Card.find(function (err, cards) {
-    if (err) return next(err);
-    res.json(cards);
-  });
-});
-
-router.get('/:id', function(req, res, next) {
-  Card.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-router.put('/:id', function(req, res, next) {
-  Card.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-*/
-
-// module.exports = router;
--- app/services/users.service.js
+++ app/services/users.service.js
@@ -3,13 +3,25 @@
 import Vue from 'vue';
 
 const usersService = {
-  model: {
-    currentUser: {},
-  },
   methods: {
-    getUser(id) {
-      id = '589263e3e7e17c0bec0cfe2b';
+    addUser(payload) {
+      let promise = Vue.http.post('/api/users', payload);
+
+      return promise;
+    },
+    getUsers(id) {
+      id = id || '589263e3e7e17c0bec0cfe2b';
       let promise = id ? Vue.http.get('/api/users/' + id) : promise = Vue.http.get('/api/users');
+
+      return promise;
+    },
+    deleteUser(id) {
+      let promise = Vue.http.delete('/api/users/' + id);
+
+      return promise;
+    },
+    updateUser(id, payload) {
+      let promise = Vue.http.put('/api/users/' + id, payload);
 
       return promise;
     }
